9f6119708dfd272a117022d85fa5a0eceaf6fba8
Adam Azizi <adamazizi101@gmail.com>
2022-09-06 17:14:43 -0400

    add Error Message for Signin

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,8 +3,8 @@ import './Register.css';
 import { FaEnvelope, FaLock, FaUser } from 'react-icons/fa';
 import CustomFunctions from '../../CustomFunctions/CustomFunctions';
 
-const { isPasswordValid, isNameValid, isEmailValid} = CustomFunctions;
-function Register({loadUser, onRouteChange}){
+const { isPasswordValid, isNameValid, isEmailValid } = CustomFunctions;
+function Register({ loadUser, onRouteChange }) {
 
     const [Name, setName] = useState('');
     const [NameError, setNameError] = useState('');
@@ -13,23 +13,15 @@ function Register({loadUser, onRouteChange}){
     const [Password, setPassword] = useState('');
     const [PasswordError, setPasswordError] = useState('');
 
+    const onRegisterNameChange = (event) => setName(event.target.value)
 
+    const onRegisterEmailChange = (event) => setEmail(event.target.value)
 
-    const onRegisterNameChange = (event) => {
-        setName(event.target.value)
-    }
+    const onRegisterPasswordChange = (event) => setPassword(event.target.value);
 
-    const onRegisterEmailChange = (event) => {
-        setEmail(event.target.value)
-    }
-
-    const onRegisterPasswordChange = (event) => { 
-        setPassword(event.target.value);
-    }
 
     const onRegisterClick = (event) => {
-        if(isPasswordValid(Password) && isNameValid(Name) && isEmailValid(Email))
-        {
+        if (isPasswordValid(Password) && isNameValid(Name) && isEmailValid(Email)) {
             setNameError('')
             setEmailError('')
             setPasswordError('')
@@ -50,62 +42,62 @@ function Register({loadUser, onRouteChange}){
                         loadUser(user);
                         onRouteChange('home');
                     }
-                    else{
+                    else {
                         setEmailError('email error')
                     }
-    
+
                 })
-        } 
-        else{
-            !isNameValid(Name)? setNameError('Name error') : setNameError('')
-            !isEmailValid(Email)? setEmailError('email error') : setEmailError('')
-            !isPasswordValid(Password)? setPasswordError('password error') : setPasswordError('')
+        }
+        else {
+            !isNameValid(Name) ? setNameError('Name error') : setNameError('')
+            !isEmailValid(Email) ? setEmailError('email error') : setEmailError('')
+            !isPasswordValid(Password) ? setPasswordError('password error') : setPasswordError('')
         }
     }
-        return (
-            <div>
-                <div className="containerRegister">
-                    <div className="screen">
-                        <div className="screen__content">
-                            <div className="RegisterForm">
-                                <div className="login__field">
-                                    <i className="login__icon"><FaUser /></i>
-                                    <input type="text" className="login__input" placeholder="Enter your Name" onChange={onRegisterNameChange} />
-                                    <p style={{color: 'red'}}>{NameError}</p>
-                                </div>
-                                <div className="login__field">
-                                    <i className="login__icon"><FaEnvelope /></i>
-                                    <input type="email" className="login__input" placeholder="Enter your Email" onChange={onRegisterEmailChange} />
-                                    <p style={{color: 'red'}}>{EmailError}</p>
-                                </div>
-                                <div className="login__field">
-                                    <i className="login__icon"><FaLock /></i>
-                                    <input type="password" className="login__input" placeholder="Enter your Password" onChange={onRegisterPasswordChange} />
-                                    <p style={{color: 'red'}}>{PasswordError}</p>
-                                </div>
-                                <button onClick={onRegisterClick} className="button login__submit">
-                                    <span className="button__text">Register</span>
-                                </button>
+    return (
+        <div>
+            <div className="containerRegister">
+                <div className="screen">
+                    <div className="screen__content">
+                        <div className="RegisterForm">
+                            <div className="login__field">
+                                <i className="login__icon"><FaUser /></i>
+                                <input type="text" className="login__input" placeholder="Enter your Name" onChange={onRegisterNameChange} />
+                                <p style={{ color: 'red' }}>{NameError}</p>
                             </div>
-                            <div className="NewMemberText">
-                                <h3>Already a Member?</h3>
-                                <div className="RegisterClick">
-                                    <p onClick={() => onRouteChange('Signin')}>Login</p>
-                                </div>
+                            <div className="login__field">
+                                <i className="login__icon"><FaEnvelope /></i>
+                                <input type="email" className="login__input" placeholder="Enter your Email" onChange={onRegisterEmailChange} />
+                                <p style={{ color: 'red' }}>{EmailError}</p>
+                            </div>
+                            <div className="login__field">
+                                <i className="login__icon"><FaLock /></i>
+                                <input type="password" className="login__input" placeholder="Enter your Password" onChange={onRegisterPasswordChange} />
+                                <p style={{ color: 'red' }}>{PasswordError}</p>
+                            </div>
+                            <button onClick={onRegisterClick} className="button login__submit">
+                                <span className="button__text">Register</span>
+                            </button>
+                        </div>
+                        <div className="NewMemberText">
+                            <h3>Already a Member?</h3>
+                            <div className="RegisterClick">
+                                <p onClick={() => onRouteChange('Signin')}>Login</p>
                             </div>
                         </div>
-                        <div className="screen__background">
-                            <span className="screen__background__shape screen__background__shape4"></span>
-                            <span className="screen__background__shape screen__background__shape3"></span>
-                            <span className="screen__background__shape screen__background__shape2"></span>
-                            <span className="screen__background__shape screen__background__shape1"></span>
-                        </div>
+                    </div>
+                    <div className="screen__background">
+                        <span className="screen__background__shape screen__background__shape4"></span>
+                        <span className="screen__background__shape screen__background__shape3"></span>
+                        <span className="screen__background__shape screen__background__shape2"></span>
+                        <span className="screen__background__shape screen__background__shape1"></span>
                     </div>
                 </div>
             </div>
+        </div>
 
 
-        );
+    );
 }
 
 export default Register;
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,84 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signin.css';
 import { FaEnvelope, FaLock, FaChevronRight } from 'react-icons/fa';
 
-class Signin extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            signInEmail: '',
-            signInPassword: ''
-        }
-    }
+function Signin({ onRouteChange, loadUser }) {
 
-    
-    onEmailChange = (event) => {
-        this.setState({ signInEmail: event.target.value })
-    }
+    const [Email, setEmail] = useState('');
+    const [EmailError, setEmailError] = useState('');
+    const [Password, setPassword] = useState('');
+    const [PasswordError, setPasswordError] = useState('');
 
-    onPasswordChange = (event) => {
-        this.setState({ signInPassword: event.target.value })
-    }
+    const onEmailChange = (event) => setEmail(event.target.value)
 
-    onSubmitSignIn = (event) => {
+    const onPasswordChange = (event) => setPassword(event.target.value)
+
+
+    const onSubmitSignIn = (event) => {
         event.preventDefault();
-        fetch('https://lit-taiga-06669.herokuapp.com/signin', {
+        // fetch('https://lit-taiga-06669.herokuapp.com/signin', {
+        fetch('http://localhost:3003/signin', {
             method: 'post',
             headers: { 'content-type': 'application/json' },
             body: JSON.stringify({
-                email: this.state.signInEmail.toLowerCase(),
-                password: this.state.signInPassword
+                email: Email.toLowerCase(),
+                password: Password
             })
 
         })
             .then(response => response.json())
             .then(user => {
                 if (user.id) {
-                    this.props.loadUser(user)
-                    this.props.onRouteChange('home')
+                    loadUser(user)
+                    onRouteChange('home')
+                }
+                else{
+                    setEmailError('Email Error')
+                    setPasswordError('Password Error')
                 }
             })
     }
 
-    render() {
-        const { onRouteChange } = this.props;
-        return (
-            <form onSubmit={this.onSubmitSignIn} action=''>
-                <div className="containerSignin">
-                    <div className="screen">
-                        <div className="screen__content">
-                            <div className="login">
-                                <div className="login__field">
-                                    <i className="login__icon"><FaEnvelope /></i>
-                                    <input onChange={this.onEmailChange} type="text" className="login__input" placeholder="Enter your Email" />
-                                </div>
-                                <div className="login__field">
-                                    <i className="login__icon"><FaLock /></i>
-                                    <input onChange={this.onPasswordChange} type="password" className="login__input" placeholder="Enter your Password" />
-                                </div>
-                                <button  type='submit' className="button login__submit">
-                                    <span>Log In Now</span>
-                                    <i className='button__icon'><FaChevronRight /></i>
-                                </button>
+    return (
+        <div>
+            <div className="containerSignin">
+                <div className="screen">
+                    <div className="screen__content">
+                        <div className="login">
+                            <div className="login__field">
+                                <i className="login__icon"><FaEnvelope /></i>
+                                <input onChange={onEmailChange} type="text" className="login__input" placeholder="Enter your Email" />
+                                <p>{EmailError}</p>
                             </div>
-                            <div className="NewMemberText">
-                                <h3>New Member?</h3>
-                                <div className="RegisterClick">
-                                    <p onClick={() => onRouteChange('register')}>Register</p>
-                                </div>
+                            <div className="login__field">
+                                <i className="login__icon"><FaLock /></i>
+                                <input onChange={onPasswordChange} type="password" className="login__input" placeholder="Enter your Password" />
+                                <p>{PasswordError}</p>
                             </div>
+                            <button onClick={onSubmitSignIn} className="button login__submit">
+                                <span>Log In Now</span>
+                                <i className='button__icon'><FaChevronRight /></i>
+                            </button>
                         </div>
-                        <div className="screen__background">
-                            <span className="screen__background__shape screen__background__shape4"></span>
-                            <span className="screen__background__shape screen__background__shape3"></span>
-                            <span className="screen__background__shape screen__background__shape2"></span>
-                            <span className="screen__background__shape screen__background__shape1"></span>
+                        <div className="NewMemberText">
+                            <h3>New Member?</h3>
+                            <div className="RegisterClick">
+                                <p onClick={() => onRouteChange('register')}>Register</p>
+                            </div>
                         </div>
                     </div>
+                    <div className="screen__background">
+                        <span className="screen__background__shape screen__background__shape4"></span>
+                        <span className="screen__background__shape screen__background__shape3"></span>
+                        <span className="screen__background__shape screen__background__shape2"></span>
+                        <span className="screen__background__shape screen__background__shape1"></span>
+                    </div>
                 </div>
-            </form>
-        );
-    }
+            </div>
+        </div>
+    );
+
 }
 
 export default Signin;
