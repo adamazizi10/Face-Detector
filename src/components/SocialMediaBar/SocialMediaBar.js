73c5edc4e6c09d74b20905429b888cbee739a7ed
Adam Azizi <adamazizi101@gmail.com>
2022-09-27 03:12:30 -0400

    add social links

--- src/components/SocialMediaBar/SocialMediaBar.js
+++ src/components/SocialMediaBar/SocialMediaBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaLinkedinIn, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
 import './SocialMediaBar.css';
 
 const SocialMediaBar = () => {
@@ -8,31 +8,20 @@ const SocialMediaBar = () => {
         <div className="mf-social-side-list">
             <ul className="socialul">
                 <li className="eaachSocialul">
-                    <a href="https://www.linkedin.com" rel="noreferrer" target="_blank">
+                    <a href="https://www.linkedin.com/in/adamazizi/" rel="noreferrer" target="_blank">
                         <i><FaLinkedinIn size={'1.8rem'} style={{marginBottom: '14px'}} /></i>
                     </a>
                 </li>
                 <li className="eaachSocialul">
-                    <a href="https://www.github.com" rel="noreferrer" target="_blank">
+                    <a href="https://github.com/adamazizi10" rel="noreferrer" target="_blank">
                         <i className='githubSpecific'><FaGithub color={'white'} size={'2.2rem'} style={{marginBottom: '22px'}}/></i>
                     </a>
                 </li>
                 <li className="eaachSocialul">
-                    <a href="https://www.youtube.com" rel="noreferrer" target="_blank">
+                    <a href="https://www.youtube.com/channel/UCynnrKNESe3gktlbUoLayHQ" rel="noreferrer" target="_blank">
                         <i className="youtubeSpecific"><FaYoutube size={'1.8rem'} style={{marginBottom: '20px'}}/></i>
                     </a>
                 </li>
-                <li className="eaachSocialul">
-                    <a href="https://www.instagram.com" rel="noreferrer" target="_blank">
-                        <i className="instagramSpecific"><FaInstagram size={'1.8rem'} style={{marginBottom: '21px'}} /></i>
-                    </a>
-                </li>
-                <li className="eaachSocialul">
-                    <a href="https://www.twitter.com"  rel="noreferrer" target="_blank">
-                        <i><FaTwitter size={'1.8rem'} style={{marginBottom: '12px'}} /></i>
-                    </a>
-                </li>
-
             </ul>
         </div>
     </div>
@@ -42,6 +31,7 @@ const SocialMediaBar = () => {
 
 
 export default SocialMediaBar;
+
 
 
 
